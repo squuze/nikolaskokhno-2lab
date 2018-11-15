337fed3282d62819714159930fc2058b63f514f8
squuze <nikolaskohno@gmail.com>
2018-11-15 13:50:07 +0300

    procedure database -> select cart user

--- nikolaskokhno@2lab/additionally/procedure/select_procedure.php
+++ nikolaskokhno@2lab/additionally/procedure/select_procedure.php
@@ -2,4 +2,7 @@
     $procedureSelectGame = mysqli_query($link, "CALL selectGameProcedure");
     $selectGameArray = mysqli_fetch_array($procedureSelectGame);
 
+    $procedureCartUser = mysqli_query($link, "CALL cartProductUser");
+    $cartProductUser = mysqli_fetch_array($procedureCartUser);
+
  ?>
